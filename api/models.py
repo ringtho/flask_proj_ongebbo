c408a57292f65cca0c42eca2932c2806e045196f
Smith Ringtho <sringtho@gmail.com>
2018-12-20 11:15:51 +0300

    add api files where input aint working

---
+++ api/models.py
@@ -0,0 +1,62 @@
+from random import randint
+from datetime import datetime
+
+
+class Users:
+    """
+    class for creating GET and POST
+    for a user
+    """
+
+    def __init__(self, firstname, lastname, othernames, email, phoneNumber, username):
+
+        self.id = randint(1,9999)
+        self.firstname = firstname
+        self.lastname = lastname
+        self.othernames = othernames
+        self.email = email
+        self.phoneNumber = phoneNumber
+        self.username = username
+        self.registered = datetime.today()
+        self.isAdmin = False
+
+
+    def json_format(self):
+        return {
+        "id": self.id,
+        "firstname": self.firstname,
+        "lastname": self.lastname,
+        "othernames": self.othernames,
+        "email": self.email,
+        "phoneNumber" : self.phoneNumber,
+        "username": self.username,
+        "registered": self.registered,
+        "isAdmin": self.isAdmin
+
+        }
+
+
+class RedFlag:
+    def __init__(self, createdBy, type, location, status, images, videos, comment):
+        self.id = randint(1,9999)
+        self.createdOn = datetime.today()
+        self.createdBy = createdBy
+        self.type = type
+        self.location = location
+        self.status = status
+        self.images = images
+        self.videos = videos
+        self.comment = comment
+
+    def json_format(self):
+        return {
+        "id": self.id,
+        "createdOn": self.createdOn,
+        "createdBy":self.createdBy,
+        "type": self.type,
+        "loctaion": self.location,
+        "status": self.status,
+        "images": self.images,
+        "videos": self.videos,
+        "comment": self.comment
+        }
---
+++ api/routes.py
@@ -0,0 +1,75 @@
+from flask import Flask, jsonify, request, make_response
+from api.models import Users
+
+
+app = Flask(__name__)
+
+
+users_list = []
+redflags = []
+
+@app.route("/")
+def hello():
+    return jsonify({"message":"Hello World"}),200
+
+@app.route("/api/v101/users", methods=["POST"])
+def create_user():
+    """
+    This function creates a new user
+    """
+    data = request.get_json()
+
+    try:
+        print(data)
+        if type(data["phoneNumber"]) is not int:
+            raise ValueError("phoneNumber  should be integer")
+        user_details = Users(data["firstname"], data["lastname"], data["othernames"],
+        data["email"], data["phoneNumber"],data["username"])
+
+        users_list.append(user_details)
+    except ValueError as e:
+        print(e)
+        return jsonify({"status": 400, "message":"phoneNumber should be an integer"}), 400
+    return jsonify({"status":201, "data": [user_details.json_format()]}),201
+
+@app.route("/api/v101/users", methods=["GET"])
+def get_users():
+    users = []
+    print(users_list)
+
+    for user in users_list:
+        users.append(user)
+
+    print(users)
+    if len(users) < 1:
+        return jsonify({"status":200,
+        "message": "There are no users created. Thanks"})
+    return jsonify({"status": 200, "data": users}), 200
+
+@app.route("/api/v101/red-flags" ,methods=["POST"])
+def add_redflag():
+    data = request.get_json()
+    try:
+        print(data)
+        if type(data["type"]) is not string:
+            raise ValueError("Red Flag type should be a string")
+        redflag = RedFlag(data["createdBy"], data["type"], data["location"],
+        data["status"], data["images"], data["videos"], data["comment"])
+        redflags.append(redflag)
+    except ValueError as e:
+        print(e)
+        return jsonify({"status": 400, "message": "Red Flag type should be a string"}), 400
+    return jsonify({"status": 201, "data": [redflags.json_format()]}), 201
+
+@app.route("/api/v101/red-flags", methods=["GET"])
+def get_redflags():
+    redflag_entry = []
+    print(redflags)
+
+    for redflag in redflags:
+        redflag_entry.append(redflag)
+
+    print(redflag_entry)
+    if len(redflag_entry) < 1:
+        return jsonify({"status": 200, "message":"There are no red flags created"}), 200
+    return jsonify({"status": 200, "data": redflag_entry }), 200
